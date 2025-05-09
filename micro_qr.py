acbd5da01fc8df9cfba598785a3628a74ee464fd
HouneTeam <130092686+houneTeam@users.noreply.github.com>
2025-05-09 14:23:21 +0900

    Update micro_qr.py

--- micro_qr.py
+++ micro_qr.py
@@ -4,7 +4,7 @@ data = "50129201"
 
 qr = segno.make_micro(data, error='M')
 
-qr.save('microqr_50129201.png', scale=10)
+qr.save('microqr.png', scale=10)
 
-# qr.save('microqr_50129201.svg')
-print("Micro QR создан и сохранён как microqr_50129201.png")
+# qr.save('microqr.svg')
+print("保存した　microqr.png")
